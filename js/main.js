f1f339045b67e716866b70734ae1e00613bc1491
Seba <gaidosebastianesteban@gmail.com>
2017-11-27 18:49:32 -0300

    agregué iconito del menú y funciones open y close en javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+
+// Open and close menu
+function openNav() {
+    document.getElementById("nav-menu").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("nav-menu").style.width = "0";
+}
