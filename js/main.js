cb6df394c293abf4c889781ff5ce030f61da23aa
Seba <gaidosebastianesteban@gmail.com>
2017-11-28 16:41:19 -0300

    cree la seccion faqs y animacion menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,27 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+// Faqs section: vertical tabs
+
+function openfaq (evt, faqItem) {
+	
+	var i, faqstab, tabcontent;
+	
+	tabcontent = document.getElementsByClassName("tabcontent");
+	
+	for (i=0;i<tabcontent.length;i++) {
+		tabcontent[i].style.display = "none";
+	}
+	
+	faqstab = document.getElementsByClassName("faqstab");
+	
+	for (i=0;i<faqstab.length;i++) {
+		faqstab[i].className = faqstab[i].className.replace(" active", "");
+	}
+	document.getElementById(faqItem).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
